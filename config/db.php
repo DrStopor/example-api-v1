a0ea11506fcaf6c95117a9025de8612c57d21707
d.rudov <rds18@mail.ru>
2023-12-30 00:32:12 +0300

    подключение к бд и секретный ключ теперь будут браться из окружения

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
 
+use yii\db\Connection;
+
+$host = getenv('DB_EXAMPLE_HOST');
+$dbname = getenv('DB_EXAMPLE_NAME');
+$username = getenv('DB_EXAMPLE_USERNAME');
+$password = getenv('DB_EXAMPLE_PASSWORD');
+
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'class' => Connection::class,
+    'dsn' => "mysql:host={$host};dbname={$dbname}",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'secretKey' => ''
+    'secretKey' => getenv('EXAMPLE_SECRET_KEY')
 ];
